37a4d362a7190b0eedd78b207c8b119f3d857791
VI-Acid <wi23b075@technikum-wien.at>
2025-03-23 13:07:12 +0100

    Footer

---
+++ includes/footer.php
@@ -0,0 +1,85 @@
+<footer class="footer bg-light border-top pt-5 pb-4">
+    <div class="container">
+        <div class="row">
+            <!-- Marke -->
+            <div class="col-12 col-xl-4 mb-4 d-flex flex-column justify-content-between">
+                <h1 class="fw-bold mb-4">ICONIQ</h1>
+            </div>
+
+            <!-- Über uns -->
+            <div class="col-12 col-md-4 col-xl-2 mb-4">
+                <h6 class="text-uppercase">About</h6>
+                <ul class="list-unstyled mt-3">
+                    <li><a href="#" class="text-decoration-none text-dark mb-2 d-block">The Brand</a></li>
+                    <li><a href="#" class="text-decoration-none text-dark mb-2 d-block">Stores</a></li>
+                    <li><a href="#" class="text-decoration-none text-dark d-block">Jobs</a></li>
+                </ul>
+            </div>
+
+            <!-- Service & Support -->
+            <div class="col-12 col-md-4 col-xl-2 mb-4">
+                <h6 class="text-uppercase">Service & Support</h6>
+                <ul class="list-unstyled mt-3">
+                    <li><a href="#" class="text-decoration-none text-dark mb-2 d-block">Size Guide</a></li>
+                    <li><a href="#" class="text-decoration-none text-dark mb-2 d-block">Newsletter</a></li>
+                    <li><a href="#" class="text-decoration-none text-dark mb-2 d-block">FAQ</a></li>
+                    <li><a href="#" class="text-decoration-none text-dark mb-2 d-block">Bezahlung</a></li>
+                    <li><a href="#" class="text-decoration-none text-dark d-block">Retoure</a></li>
+                </ul>
+            </div>
+
+            <!-- Kontakt -->
+            <div class="col-12 col-md-4 col-xl-2 mb-4">
+                <h6 class="text-uppercase">Kontakt</h6>
+                <ul class="list-unstyled mt-3">
+                    <li><a href="mailto:support@myoakshop.com" class="text-decoration-none text-dark mb-2 d-block">support@iconiq.com</a></li>
+                    <li class="text-muted small mb-2">Mo - Fr von 09:00 bis 18:00 Uhr</li>
+                    <li><a href="#" class="text-decoration-none text-dark d-block">Zum Kontaktformular</a></li>
+                </ul>
+            </div>
+
+
+
+            <hr class="my-4">
+
+            <!-- Footer Bottom -->
+            <div class="row">
+                <div class="col-6">
+                    <span>AT | <span class="text-capitalize">Deutsch</span></span>
+                </div>
+                <div class="col-6 text-end">
+                    <a href="#" class="text-decoration-none text-dark me-3">Cookie Einstellungen</a>
+                    <a href="#" class="text-decoration-none text-dark me-3">AGB</a>
+                    <a href="#" class="text-decoration-none text-dark me-3">Datenschutz</a>
+                    <a href="#" class="text-decoration-none text-dark">Impressum</a>
+                </div>
+            </div>
+        </div>
+</footer>
+
+
+
+<style>
+    /* Roboto Flex von Google Fonts einbinden */
+    @import url('https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap');
+
+    .footer {
+        font-family: 'Roboto Flex', sans-serif;
+        font-size: 10px;
+        /* Kleinere Schriftgröße */
+    }
+
+    h1 {
+        font-size: 24px;
+    }
+
+    h6 {
+        font-family: 'Roboto Flex', sans-serif;
+        font-size: 11px;
+    }
+
+
+    footer a:hover {
+        text-decoration: underline;
+    }
+</style>
