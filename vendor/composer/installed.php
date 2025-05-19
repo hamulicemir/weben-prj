9d965af067eed2703bd899d665d38f9fd2057392
VI-Acid <wi23b075@technikum-wien.at>
2025-05-19 14:05:15 +0200

    Composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '65ae14f1cf36615418ade7b8ac48297da56b7691',
+        'reference' => 'd7e4a9972c76b6506005fd308c084a16166295bf',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '65ae14f1cf36615418ade7b8ac48297da56b7691',
+            'reference' => 'd7e4a9972c76b6506005fd308c084a16166295bf',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
